f59444fc340480cc6c40d2ce4001a49879ff9dad
Andrea Centeno <andreacent8@gmail.com>
2017-03-27 20:25:55 -0400

    imagetarget correctas

---
+++ Assets/Script/TrackableList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class TrackableList : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// Get the Vuforia StateManager
+		StateManager sm = TrackerManager.Instance.GetStateManager ();
+
+		// Query the StateManager to retrieve the list of
+		// currently 'active' trackables 
+		//(i.e. the ones currently being tracked by Vuforia)
+		IEnumerable<TrackableBehaviour> activeTrackables = sm.GetActiveTrackableBehaviours ();
+
+		// Iterate through the list of active trackables
+		Debug.Log ("List of trackables currently active (tracked): ");
+		foreach (TrackableBehaviour tb in activeTrackables) {
+			Debug.Log("Trackable: " + tb.TrackableName);
+		}
+	}
+}
