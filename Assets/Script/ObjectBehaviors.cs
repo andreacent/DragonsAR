a36852f4dd86a08f0aab5ed8bd3dbbdd5539da18
Andrea Centeno <andreacent8@gmail.com>
2017-03-27 23:21:19 -0400

    modifica al personaje segun la marca mostrada

---
+++ Assets/Script/ObjectBehaviors.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ObjectBehaviors : MonoBehaviour,ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+	public GameObject knightAttr;
+
+	void Start(){
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour){
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+     
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,
+                                    	TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED){
+			knightAttr.SetActive(true);
+        }
+        else{
+			knightAttr.SetActive(false);
+        }
+    }   
+}
+
