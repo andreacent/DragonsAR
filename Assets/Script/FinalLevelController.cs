a759205a14dfae9e6ccd63124f200a27ecbbf2c0
Andrea Centeno <andreacent8@gmail.com>
2017-03-28 10:06:47 -0400

    animaciones del nivel final

---
+++ Assets/Script/FinalLevelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Vuforia;
+
+public class FinalLevelController: MonoBehaviour {
+	public static FinalLevelController Instance { get; private set; }
+
+	public bool win = false;
+	public Text textGame;
+	private GameObject player;
+	private GameObject dragon;
+
+	void Awake() {
+		textGame.text = "";
+
+		if (null == Instance) Instance = this;
+		else {
+			Debug.LogError("FinalLevelController::Awake - Instance already set. Is there more than one in scene?");
+		}
+	}
+
+	void Start (){
+		player = GameObject.FindWithTag("Player");
+		dragon = GameObject.FindWithTag("Dragon");
+	}         
+
+	// Update is called once per frame
+	void Update () {
+		if(ErrorManager.error > 2) StartCoroutine("LoseSecene"); 
+		else if(win) StartCoroutine("WinScene"); 
+	}
+
+	IEnumerator WinScene() {
+		textGame.text = "Muy Bien!!!";
+		player.GetComponent<Animator>().Play("Taunt");
+		yield return new WaitForSeconds(2);
+		dragon.GetComponent<Animator>().Play("dead");
+		yield return new WaitForSeconds(1);
+		player.GetComponent<Animator>().Play("win");
+	}
+
+	IEnumerator LoseSecene() {
+		textGame.text = "Perdiste!";
+		dragon.GetComponent<Animator>().Play("attack");
+		yield return new WaitForSeconds(1);
+		player.GetComponent<Animator>().Play("died");
+	}
+}
---
+++ Assets/Script/FinalLevelObjBehaviors.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections;
+
+public class FinalLevelObjBehaviors : MonoBehaviour,ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+	public GameObject knightAttr;
+	public bool isSol = false;
+
+	void Start(){
+		mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+		if (mTrackableBehaviour){
+			mTrackableBehaviour.RegisterTrackableEventHandler(this);
+		}
+	}
+
+	public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED){
+			knightAttr.SetActive(true);
+
+			if (null == FinalLevelController.Instance)  return;
+
+			if(isSol) FinalLevelController.Instance.win = true;
+			else StartCoroutine("DesactiveAttr"); 
+		}
+		else{
+			knightAttr.SetActive(false);
+		}
+	}   
+
+	IEnumerator DesactiveAttr() {
+		yield return new WaitForSeconds(1);
+		knightAttr.SetActive(false);
+		ErrorManager.error++;
+	}
+}
+
+
+
